5ca2c86a1a0ef06676a2f652e53244236b580d9b
Nycht1 <nasutionrachmat@gmail.com>
2024-06-13 21:14:24 +0700

    temporary catalogue view

--- app/Views/pages/catalogue.php
+++ app/Views/pages/catalogue.php
@@ -1,7 +1,46 @@
 <?= $this->extend('layout/template') ?>
 
 <?= $this->section('content') ?>
-<div class="container">
+<?= $this->extend('layout/template') ?>
+
+<?= $this->section('content') ?>
+<!-- Recent Posts Section -->
+<section id="recent-posts" class="recent-posts section">
+    <!-- Section Title -->
+    <div class="container section-title mt-4" data-aos="fade-up">
+        <h2>Our Catalogue</h2>
+        <p>
+            Check out what we has to offer.
+        </p>
+    </div>
+    <!-- End Section Title -->
+
+    <div class="container">
+        <div class="row gy-4">
+            <?php foreach ($packages as $package) : ?>
+                <div class="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                    <article>
+                        <div class="post-img">
+                            <img src="<?= base_url("uploads/package/{$package['image']}") ?>" alt="" class="img-fluid" />
+                        </div>
+
+                        <h2 class="title">
+                            <a href="blog-details.html"><?= $package['name'] ?></a>
+                        </h2>
+                        <p class="post-category"><?= $package['price'] ?></p>
+                    </article>
+                </div>
+                <!-- End post list item -->
+            <?php endforeach; ?>
+        </div>
+        <!-- End recent posts list -->
+    </div>
+</section>
+<!-- /Recent Posts Section -->
+
+<?= $this->endSection() ?>
+<?= $this->endSection() ?>
+<!-- <div class="container">
     <h1>Wedding Packages</h1>
     <div class="row">
         <?php foreach ($packages as $package) : ?>
@@ -17,6 +56,4 @@
             </div>
         <?php endforeach; ?>
     </div>
-</div>
-
-<?= $this->endSection() ?>
+</div> -->
