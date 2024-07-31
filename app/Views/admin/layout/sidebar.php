659d27fce313c27d746b6a330f512150cd88130d
Nycht1 <nasutionrachmat@gmail.com>
2024-07-31 14:08:09 +0700

    fix broken link

--- app/Views/admin/layout/sidebar.php
+++ app/Views/admin/layout/sidebar.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="dashboard" class="brand-link py-4">
+    <a href="<?= base_url('admin/dashboard') ?>" class="brand-link py-4">
         <img src="<?= base_url('assets/') ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">JWO Dashboard</span>
     </a>
